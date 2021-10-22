6a8b4a01d1d4688b861b6c3a7cc182569db1b366
Verity <joshua-schmidt@online.de>
2021-10-22 21:24:46 +0200

    :art: Convert spin button to component

---
+++ src/main/java/userinterface/components/GradientButtonComponent.java
@@ -0,0 +1,166 @@
+package userinterface.components;
+
+import userinterface.Component;
+import userinterface.WindowHandler;
+import userinterface.screens.HomeScreen;
+import utils.CustomFont;
+import utils.Logger;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+public class GradientButtonComponent extends Component {
+
+    public static String text;
+    private final int arc;
+    public boolean isRotating = false;
+    public int rotation = 0;
+    private HomeScreen homeScreen = null;
+    private final BufferedImage image;
+    private final Color gradientStart;
+    private final Color gradientEnd;
+    private final int fontSize;
+    private boolean isHovered = false;
+
+    public GradientButtonComponent(int x, int y, int width, int height, String text, int fontSize, BufferedImage image, Color gradientStart, Color gradientEnd, int arc) {
+        super(x, y, width, height);
+        this.image = resize(image, 25, 25);
+        this.fontSize = fontSize;
+        GradientButtonComponent.text = text;
+        this.gradientStart = gradientStart;
+        this.gradientEnd = gradientEnd;
+        this.arc = arc;
+    }
+
+    private static BufferedImage resize(BufferedImage img, int width, int height) {
+        Image temp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = image.createGraphics();
+        g2d.drawImage(temp, 0, 0, null);
+        g2d.dispose();
+
+        return image;
+    }
+
+    public void rotateImage() {
+        if (!isRotating) {
+            isRotating = true;
+
+            new Thread() { // Spin image rotation animation (Runs once per spin-start)
+                @Override
+                public void run() {
+                    while (true) {
+                        if (homeScreen.currentlySpinning) {
+                            try {
+                                Thread.sleep(45);
+                                rotation++;
+                            } catch (Exception e) {
+                                Logger.error(e.getMessage());
+                            }
+                        } else {
+                            rotation = 0;
+                            isRotating = false;
+                            break;
+                        }
+                    }
+                }
+            }.start();
+        }
+    }
+
+    @Override
+    public void draw(Graphics g, ImageObserver observer) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        if (homeScreen == null) {
+            homeScreen = (HomeScreen) WindowHandler.screen;
+        }
+
+        if (WindowHandler.screen instanceof HomeScreen) {
+            g2d.setPaint(new GradientPaint(x, y + (height / 2), gradientStart, x + width, y + (height / 2), gradientEnd));
+            g.fillRoundRect(x, y, width, height, arc, arc);
+
+            if (isHovered) {
+                g.setColor(Color.black);
+                g2d.setStroke(new BasicStroke(2f));
+                g.drawRoundRect(x, y, width, height, 50, 50);
+            }
+
+
+            AffineTransform transform = new AffineTransform();
+            transform.translate(x + 140, y + (image.getWidth() / 2));
+            transform.rotate(rotation, (image.getWidth() / 2), (image.getHeight() / 2));
+
+            g2d.drawImage(image, transform, observer);
+
+            g.setColor(Color.white);
+            g.setFont(CustomFont.light.deriveFont((float) fontSize));
+
+            int stringWidth = g.getFontMetrics().stringWidth(text);
+            int stringHeight = g.getFontMetrics().getHeight();
+
+            g.drawString(text, x + (width - stringWidth) / 2, y + (height - stringHeight) / 2 + g.getFontMetrics().getAscent());
+
+        }
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        if (e.getX() > 235 && e.getX() < 425 && e.getY() > 690 && e.getY() < 740) {
+            WindowHandler.window.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            if (!homeScreen.isSpinOutlineAnimationRunning) {
+                isHovered = true;
+            }
+        } else {
+            WindowHandler.window.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            if (!homeScreen.isSpinOutlineAnimationRunning) {
+                isHovered = false;
+            }
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getX() > 235 && e.getX() < 425 && e.getY() > 690 && e.getY() < 740) {
+            if (!homeScreen.currentlySpinning) {
+                homeScreen.animateSpinImage();
+            }
+
+            for (Component component : homeScreen.components) {
+                if (component instanceof SpinWheelComponent) {
+                    SpinWheelComponent wheel = ((SpinWheelComponent) component);
+                    if (!wheel.isRolling) {
+                        Logger.trace("Start spin wheel #" + wheel.index);
+                        wheel.roll();
+                    }
+                }
+            }
+
+            new Thread() { // Spin buttons outline animation + interrupting result highlighter animation (Runs once per spin-start)
+                @Override
+                public void run() {
+                    try {
+                        homeScreen.isSpinOutlineAnimationRunning = true;
+                        isHovered = false;
+                        Thread.sleep(250);
+                        isHovered = true;
+                        homeScreen.isSpinOutlineAnimationRunning = false;
+
+                        homeScreen.interruptResultHighlighterAnimation = true;
+                        Thread.sleep(500);
+                        homeScreen.interruptResultHighlighterAnimation = false;
+
+                        Thread.currentThread().interrupt();
+                    } catch (Exception ex) {
+                        Logger.error(ex.getMessage());
+                    }
+                }
+            }.start();
+
+        }
+    }
+}
--- src/main/java/userinterface/screens/HomeScreen.java
+++ src/main/java/userinterface/screens/HomeScreen.java
@@ -2,7 +2,7 @@ package userinterface.screens;
 
 import userinterface.Component;
 import userinterface.Screen;
-import userinterface.WindowHandler;
+import userinterface.components.GradientButtonComponent;
 import userinterface.components.SpinWheelComponent;
 import utils.CustomFont;
 import utils.Logger;
@@ -10,29 +10,30 @@ import utils.Logger;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class HomeScreen extends Screen {
 
     public ArrayList<SpinWheelComponent> finishedWheels = new ArrayList<>();
+    public boolean isSpinOutlineAnimationRunning = false;
+    public boolean interruptResultHighlighterAnimation = false;
+    public boolean currentlySpinning = false;
     private boolean drawResultHighlighter = true;
-    private boolean isSpinButtonHovered = false;
-    private boolean isSpinOutlineAnimationRunning = false;
     private boolean isResultHighlighterAnimationRunning = false;
-    private boolean interruptResultHighlighterAnimation = false;
-    private boolean currentlySpinning = false;
-
-    private int spinImageRotation = 0;
 
     public HomeScreen() {
         SpinWheelComponent firstWheel = new SpinWheelComponent(1, 100, 180, 50, 50);
         SpinWheelComponent secondWheel = new SpinWheelComponent(2, 255, 180, 50, 50);
         SpinWheelComponent thirdWheel = new SpinWheelComponent(3, 410, 180, 50, 50);
-
+        try {
+            GradientButtonComponent spinButton = new GradientButtonComponent(230, 665, 180, 50, "SPIN", 24, ImageIO.read(new File("assets/images/home/spin.png")), Color.decode("#4834D4"), Color.decode("#B500FF"), 50);
+            components.add(spinButton);
+        } catch (IOException e) {
+            Logger.warn("Unable to read spin image (" + e.getMessage() + ")");
+        }
         components.add(firstWheel);
         components.add(secondWheel);
         components.add(thirdWheel);
@@ -56,33 +57,24 @@ public class HomeScreen extends Screen {
 
     }
 
-    public static BufferedImage resize(BufferedImage img, int width, int height) {
-        Image temp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-
-        Graphics2D g2d = image.createGraphics();
-        g2d.drawImage(temp, 0, 0, null);
-        g2d.dispose();
-
-        return image;
-    }
-
     @Override
     public void draw(Graphics g, ImageObserver observer) {
         Graphics2D g2d = (Graphics2D) g;
 
         for (Component component : components) {
-            component.draw(g, observer);
+            if (component instanceof SpinWheelComponent) {
+                component.draw(g, observer);
+            }
         }
 
         /* RESULT RECT */
 
         if (drawResultHighlighter) {
             int rectWidth = (3 * 125) + (30 * 2) + 20;
-            if(currentlySpinning) {
+            if (currentlySpinning) {
                 AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f);
                 g2d.setComposite(composite);
-            }else {
+            } else {
                 AlphaComposite composite = AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f);
                 g2d.setComposite(composite);
             }
@@ -106,112 +98,41 @@ public class HomeScreen extends Screen {
         g.setFont(CustomFont.medium.deriveFont(40f));
         g.drawString("SLOT MACHINE", 205, 100);
 
-        /* SPIN BUTTON */
+        /* COMPONENTS */
 
-        g2d.setPaint(new GradientPaint(230, 715, Color.decode("#4834D4"), 410, 715, Color.decode("#B500FF")));
-        g.fillRoundRect(230, 665, 180, 50, 50, 50);
-
-        if (isSpinButtonHovered) {
-            g.setColor(Color.black);
-            g2d.setStroke(new BasicStroke(2f));
-            g.drawRoundRect(230, 665, 180, 50, 50, 50);
+        for (Component component : components) {
+            if (!(component instanceof SpinWheelComponent)) {
+                component.draw(g, observer);
+            }
         }
-
-        try {
-            BufferedImage image = resize(ImageIO.read(new File("assets/images/home/spin.png")), 25, 25);
-
-            AffineTransform transform = new AffineTransform();
-            transform.translate(370, 677);
-            transform.rotate(Math.toRadians(spinImageRotation), 12, 12);
-
-            g2d.drawImage(image, transform, observer);
-        } catch (Exception e) {
-            Logger.warn("Unable to read spin image (" + e.getMessage() + ")");
-        }
-
-        g.setColor(Color.white);
-        g.setFont(CustomFont.light.deriveFont(24f));
-        g.drawString("SPIN", 295, 698);
 
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        if (e.getX() > 235 && e.getX() < 425 && e.getY() > 690 && e.getY() < 740) {
-            WindowHandler.window.setCursor(new Cursor(Cursor.HAND_CURSOR));
-            if (!isSpinOutlineAnimationRunning) {
-                isSpinButtonHovered = true;
-            }
-        } else {
-            WindowHandler.window.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-            if (!isSpinOutlineAnimationRunning) {
-                isSpinButtonHovered = false;
-            }
+        for (Component component : components) {
+            component.mouseMoved(e);
         }
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        if (e.getX() > 235 && e.getX() < 425 && e.getY() > 690 && e.getY() < 740) {
-            if (!currentlySpinning) {
-                animateSpinImage();
-            }
-
-            for (Component component : components) {
-                if (component instanceof SpinWheelComponent) {
-                    SpinWheelComponent wheel = ((SpinWheelComponent) component);
-                    if (!wheel.isRolling) {
-                        Logger.trace("Start spin wheel #" + wheel.index);
-                        wheel.roll();
-                    }
-                }
-            }
-
-            new Thread() { // Spin buttons outline animation + interrupting result highlighter animation (Runs once per spin-start)
-                @Override
-                public void run() {
-                    try {
-                        isSpinOutlineAnimationRunning = true;
-                        isSpinButtonHovered = false;
-                        Thread.sleep(250);
-                        isSpinButtonHovered = true;
-                        isSpinOutlineAnimationRunning = false;
-
-                        interruptResultHighlighterAnimation = true;
-                        Thread.sleep(500);
-                        interruptResultHighlighterAnimation = false;
-
-                        Thread.currentThread().interrupt();
-                    } catch (Exception ex) {
-                        Logger.error(ex.getMessage());
-                    }
-                }
-            }.start();
-
+        for (Component component : components) {
+            component.mouseClicked(e);
         }
     }
 
-    private void animateSpinImage() {
+    public void animateSpinImage() {
         currentlySpinning = true;
 
-        new Thread() { // Spin image rotation animation (Runs once per spin-start)
-            @Override
-            public void run() {
-                while (true) {
-                    if (currentlySpinning) {
-                        try {
-                            Thread.sleep(2);
-                            spinImageRotation++;
-                        } catch (Exception e) {
-                            Logger.error(e.getMessage());
-                        }
-                    } else {
-                        spinImageRotation = 0;
-                        break;
-                    }
+        for (Component component : components) {
+            if (component instanceof GradientButtonComponent) {
+                GradientButtonComponent gradientButtonComponent = (GradientButtonComponent) component;
+                if (GradientButtonComponent.text.equals("SPIN")) {
+                    gradientButtonComponent.rotateImage();
                 }
             }
-        }.start();
+        }
     }
 
     private void animateResultHighlighter() {
