d816d80bc435d0a3a8d0a59219bcc7e7d52d34d2
Verity <joshua-schmidt@online.de>
2021-10-25 20:36:47 +0200

    :sparkles: Add enable/disable switch for logger colors (for consoles which doesnt support asci color codes)

--- src/main/java/utils/Logger.java
+++ src/main/java/utils/Logger.java
@@ -11,6 +11,8 @@ public class Logger {
     private static final boolean enableLogic = true;
     private static final boolean enableUI = true;
 
+    private static final boolean enableColors = true;
+
     private static final String ANSI_RESET = "\u001B[0m";
     private static final String ANSI_RED = "\u001B[31m";
     private static final String ANSI_GREEN = "\u001B[32m";
@@ -21,49 +23,77 @@ public class Logger {
     private static final String ANSI_GREY = "\u001B[90m";
 
     public static void animation(String text) {
-        if (enableAnimation) {
-            String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_YELLOW + "ANIMATION" + ANSI_GREY + "] " + ANSI_RESET + text);
+        String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        if (enableColors) {
+            if (enableAnimation) {
+                System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_YELLOW + "ANIMATION" + ANSI_GREY + "] " + ANSI_RESET + text);
+            }
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
         }
     }
 
     public static void logic(String text) {
-        if (enableLogic) {
-            String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_BLUE + "LOGIC" + ANSI_GREY + "] " + ANSI_RESET + text);
+        String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        if (enableColors) {
+            if (enableLogic) {
+                System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_BLUE + "LOGIC" + ANSI_GREY + "] " + ANSI_RESET + text);
+            }
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
         }
     }
 
     public static void ui(String text) {
-        if (enableUI) {
-            String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_GREEN + "UI" + ANSI_GREY + "] " + ANSI_RESET + text);
+        String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        if (enableColors) {
+            if (enableUI) {
+                System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_GREEN + "UI" + ANSI_GREY + "] " + ANSI_RESET + text);
+            }
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
         }
     }
 
     public static void debug(String text) {
-        if (enableDebug) {
-            String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_CYAN + "DEBUG" + ANSI_GREY + "] " + ANSI_RESET + text);
+        String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        if (enableColors) {
+            if (enableDebug) {
+                System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_CYAN + "DEBUG" + ANSI_GREY + "] " + ANSI_RESET + text);
+            }
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
         }
     }
 
     public static void trace(String text) {
-        if (enableTrace) {
-            String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_PURPLE + "TRACE" + ANSI_GREY + "] " + ANSI_RESET + text);
+        String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
+        if (enableColors) {
+            if (enableTrace) {
+                System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_PURPLE + "TRACE" + ANSI_GREY + "] " + ANSI_RESET + text);
+            }
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
         }
     }
 
     public static void warn(String text) {
         String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-        System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_YELLOW + "WARN" + ANSI_GREY + "] " + ANSI_RESET + text);
+        if (enableColors) {
+            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_YELLOW + "WARN" + ANSI_GREY + "] " + ANSI_RESET + text);
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
+        }
     }
 
     public static void error(String text) {
         String date = new SimpleDateFormat("HH:mm:ss").format(new Date());
-        System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_RED + "ERROR" + ANSI_GREY + "] " + ANSI_RESET + text);
-        System.exit(-1);
+        if (enableColors) {
+            System.out.println(ANSI_GREY + "[" + date + "]: [" + ANSI_RED + "ERROR" + ANSI_GREY + "] " + ANSI_RESET + text);
+            System.exit(-1);
+        } else {
+            System.out.println("[" + date + "]: [ANIMATION] " + text);
+        }
     }
 
 }
